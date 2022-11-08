0115b291a3363781475a5db194f9ebd93882f283
Satoe Sakonjo <sakonjo310@gmail.com>
2022-11-07 22:43:37 -0800

    Implement a function to add tweets to the feed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,65 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
+
+const renderTweets = function(tweetsArr) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    return tweetsArr.forEach(tweet => {
+        $('#tweet-container').append(createTweetElement(tweet));
+      });
+}
+
+const createTweetElement = function(tweet) {
+    let $tweet = `
+    <article class="feed">
+        <header>
+        <div class="icon">
+            <img src="${tweet.user.avatars}">
+            <p>${tweet.user.name}</p>
+        </div>
+        <p class="username">${tweet.user.handle}</p>
+        </header>
+        <p class="tweet">${tweet.content.text}</p>
+        <footer>
+        <p>${tweet.created_at}</p>
+        <div class="action-icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+        </div>
+        </footer>
+    </article>`
+    return $tweet;
+}
+
+renderTweets(data);
+
+});
