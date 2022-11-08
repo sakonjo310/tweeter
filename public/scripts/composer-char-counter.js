9b936c743110d2ae9775726d7d0702277136b763
Satoe Sakonjo <sakonjo310@gmail.com>
2022-11-08 08:05:03 -0800

    Store maximum tweet length in a variable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     const $textarea = $('#tweet-text')
     $textarea.on('input', function() {
+        const maxLength = 140;
         const tweetLength = $(this).val().length;
-        $(this).nextAll('.counter').text(140 - tweetLength);
-        if (tweetLength > 140) {
+        $(this).nextAll('.counter').text(maxLength - tweetLength);
+        if (tweetLength > maxLength) {
             $(this).nextAll('.counter').css("color", "red");
         }
     })
